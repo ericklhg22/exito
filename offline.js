64a554cd9e5e419dbb67090ae566f046d3997713
ericklhg22 <73855575+ericklhg22@users.noreply.github.com>
2021-03-12 11:08:37 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,11 @@
+﻿{
+	"version": 1615562411,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png"
+	]
+}
